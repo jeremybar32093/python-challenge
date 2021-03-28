dae788dde9193f80faffb3d4d88eaf1c77f99b15
Jeremy Bar <jeremybar@live.com>
2021-03-28 14:57:40 -0400

    Updated PyBank main.py - added Total profit/loss calculation

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,7 @@ csvpath = os.path.join('Resources','budget_data.csv')
 
 # Create initial counter for total # of months in file
 total_months = 0
+total_profit_loss = 0
 
 # Open csv file in read mode
 with open(csvpath) as csvfile:
@@ -16,8 +17,12 @@ with open(csvpath) as csvfile:
     next(csvreader)
     # Iterate through csvreader data records and perform relevant calculations
     for row in csvreader:
+        # total months in file
         total_months += 1
+        # total profit/losses in each file
+        total_profit_loss += float(row[1])
 
 # Print output results
 print("Financial Analysis\n---------------------------")
 print(f"Total Months: {total_months}")
+print(f"Total: ${int(total_profit_loss)}")
