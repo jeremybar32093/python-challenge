2c8d3212852fd079b6b76147641149306ee5597a
Jeremy Bar <jeremybar@live.com>
2021-03-30 21:06:21 -0400

    Added PyParagraph folder and corresponding main.py file

---
+++ PyParagraph/main.py
@@ -0,0 +1,9 @@
+import os
+import re
+
+# Define path for output analysis file
+outputpath = os.path.join('analysis','paragraph_analysis.txt')
+
+# Paragraph pulled from https://www.penguinrandomhouse.ca/books/108336/a-game-of-thrones-hbo-tie-in-edition-by-george-r-r-martin/9780553386790/excerpt
+paragraph = "The morning had dawned clear and cold, with a crispness that hinted at the end of summer. They set forth at daybreak to see a man beheaded, twenty in all, and Bran rode among them, nervous with excitement. This was the first time he had been deemed old enough to go with his lord father and his brothers to see the king's justice done. It was the ninth year of summer, and the seventh of Bran's life."
+
